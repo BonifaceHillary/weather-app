a97f973b34a96cae2e1a5b0fe97cae7035792d66
Boniface Hillary <nkasiboniface@gail.com>
2025-12-06 10:45:57 +0100

    initial commit

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ const apiKey = "0c766df5fb31d773daebe25a36b7e9cb";
     }
     else if(data.weather[0].main == "Rain"){
       weatherIcon.src = "images/sun2.webp";
-
     }
     else if(data.weather[0].main == "Drizzle"){
       weatherIcon.src = "images/sun3.png";
