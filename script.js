9060a2554c074715ba677b71a816d6861eb96779
Boniface Hillary <nkasiboniface@gail.com>
2026-01-17 09:50:12 +0100

    initial commit

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const apiKey = "0c766df5fb31d773daebe25a36b7e9cb";
 
     async function checkWeather(city) {
       const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
       if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none"
